a475cefe2b0940924334e4dec3590c482fdbd9f4
OnSmoko <80360013+NZDurriez@users.noreply.github.com>
2025-08-19 09:57:12 +1200

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ The Beehive Staff Team conduct monthly reviews of members who are on 3SP, assess
 - The timeframe for being on 3SP is not predetermined; however, demonstrating good behavior and engaging in positive roleplay may lead to its removal sooner.  
 - Community Rule 11 always applies.  
 
-If you have any questions or comments, feel free to share them below. Otherwise, please react to this message with a ✅, and we will close the ticket.
+If you have any questions or comments, feel free to share them below. Otherwise, please react to this message with a ✅ and we will close the ticket.
 
 Kind Regards,  
 ${staffMember},  
